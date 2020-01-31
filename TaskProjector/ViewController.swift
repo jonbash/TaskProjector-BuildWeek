67f67b8752544f3cbe9d84e1ddff9846d847b7cc
Jon Bash <jonbash@protonmail.com>
2020-01-31 15:10:28 -0800

    init xcode project

---
+++ TaskProjector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskProjector
+//
+//  Created by Jon Bash on 2020-01-31.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
