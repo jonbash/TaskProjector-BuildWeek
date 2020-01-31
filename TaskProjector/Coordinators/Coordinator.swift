24e7e816207a8dfae56fee6247cc30d3be7cbfb7
Jon Bash <jonbash@protonmail.com>
2020-01-31 15:34:05 -0800

    set up main coordinator

---
+++ TaskProjector/Coordinators/Coordinator.swift
@@ -0,0 +1,16 @@
+//
+//  Coordinator.swift
+//  TaskProjector
+//
+//  Created by Jon Bash on 2020-01-31.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+
+    func start()
+}
---
+++ TaskProjector/Coordinators/MainCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  MainCoordinator.swift
+//  TaskProjector
+//
+//  Created by Jon Bash on 2020-01-31.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        // let vc = ViewController.instantiate()
+        // navigationController.pushViewController(vc, animated: false)
+    }
+}
