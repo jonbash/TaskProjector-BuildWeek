dd097b67d8c19afa84fd29c035e7bc46ee73e467
Jon Bash <jonbash@protonmail.com>
2020-02-01 17:14:23 -0800

    set up some colors

---
+++ TaskProjector/Views/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  TaskProjector
+//
+//  Created by Jon Bash on 2020-02-01.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
