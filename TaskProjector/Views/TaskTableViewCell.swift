73b3edf8dc00c38af3be16229ddc8c7808b4a4aa
Jon Bash <jonbash@protonmail.com>
2020-02-01 17:14:56 -0800

    set up taskTableViewCell

--- TaskProjector/Views/TaskTableViewCell.swift
+++ TaskProjector/Views/TaskTableViewCell.swift
@@ -10,15 +10,33 @@ import UIKit
 
 class TaskTableViewCell: UITableViewCell {
 
+    @IBOutlet private weak var checkmarkButton: UIButton!
+    @IBOutlet private weak var taskNameLabel: UILabel!
+
+    var task: Task!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        updateViews()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-    
+
+    func updateViews() {
+        checkmarkButton.setBackgroundImage(task.state.image, for: .normal)
+        taskNameLabel.text = task.name
+    }
+
+    @IBAction func completeButtonTapped(_ sender: UIButton) {
+        task.toggleComplete()
+        updateViews()
+    }
+
+    @IBAction func completeButtonLongPressed(
+        _ sender: UILongPressGestureRecognizer
+    ) {
+        print("long press")
+    }
 }
