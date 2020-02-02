053ec5f82c338a869b27e3fc046c8f5c6bdc41cc
Jon Bash <jonbash@protonmail.com>
2020-02-01 17:14:33 -0800

    add color methods to state

--- TaskProjector/Models/Enums/CompletableState.swift
+++ TaskProjector/Models/Enums/CompletableState.swift
@@ -6,11 +6,38 @@
 //  Copyright © 2020 Jon Bash. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 enum CompletableState: String {
     case active = "Active"
     case onHold = "On Hold"
     case done = "Done"
     case dropped = "Dropped"
+
+    // TODO: Unit test assert not-nil for each
+    var image: UIImage {
+        let imageName: String = {
+            switch self {
+            case .active: return "square"
+            case .onHold: return "lessthan.square"
+            case .done: return "checkmark.square"
+            case .dropped: return "xmark.square"
+            }
+        }()
+        return UIImage(systemName: imageName)!
+    }
+
+    // TODO: Unit test assert not-nil for each
+    var color: UIColor {
+        // TODO:
+        let colorName: String = {
+            switch self {
+            case .active: return "TaskActive"
+            case .onHold: return "TaskInactive"
+            case .done: return "TaskInactive"
+            case .dropped: return "TaskInactive"
+            }
+        }()
+        return UIColor(named: colorName)!
+    }
 }
