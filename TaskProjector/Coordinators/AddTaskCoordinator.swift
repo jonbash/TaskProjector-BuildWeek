b63343cf882c82c8e0e5cedbedd8d8a543005bf0
Jon Bash <jonbash@protonmail.com>
2020-02-03 12:58:25 -0800

    add addTaskCoordinator & multiple VCs maybe??

---
+++ TaskProjector/Coordinators/AddTaskCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  AddTaskCoordinator.swift
+//  TaskProjector
+//
+//  Created by Jon Bash on 2020-02-03.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskCoordinator: Coordinator {
+    var childCoordinators: [Coordinator] = []
+
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        navigationController.pushViewController(AddTaskViewController(), animated: true)
+    }
+}
