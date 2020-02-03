d7832416259fb7b0786edc205c2b5482285817db
Jon Bash <jonbash@protonmail.com>
2020-02-03 08:01:26 -0800

    add shiftableVC, addTaskVC

---
+++ TaskProjector/Views/View Controllers/AddTaskViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddTaskViewController.swift
+//  TaskProjector
+//
+//  Created by Jon Bash on 2020-02-03.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: ShiftableViewController {
+    
+}
